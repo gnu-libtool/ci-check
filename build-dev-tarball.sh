b18dee2c9a2db01fa99edb0cf646d376b6f24683
Ileana Dumitrescu <ileanadumitrescu95@gmail.com>
2024-08-19 22:27:51 +0300

    [solaris] [pre-release] Syntax fix?

--- build-dev-tarball.sh
+++ build-dev-tarball.sh
@@ -21,6 +21,7 @@
 
 package="$1"
 branch="$2"
+commit_message="$3"
 
 if test -z $branch; then
     branch="master"
@@ -56,3 +57,7 @@ date --utc --iso-8601 > .tarball-version
 make > log2 2>&1; rc=$?; cat log2; test $rc = 0 || exit 1
 # Check that tarballs are correct.
 make distcheck TESTSUITEFLAGS="-d 1" > log4 2>&1; rc=$?; cat log4; test $rc = 0 || exit 1
+
+if [[ "$commit_message" == *"[pre-release]"* ]]; then
+  echo "SUCCESS"
+fi
