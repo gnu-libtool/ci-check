e7dd1d241c5ae9d208058e07d91b842d2683fde7
Ileana Dumitrescu <ileanadumitrescu95@gmail.com>
2024-10-02 19:59:54 +0300

    [macos] Test of older gnulib

--- build-dev-tarball.sh
+++ build-dev-tarball.sh
@@ -38,6 +38,8 @@ set -e
 git clone --depth 2 -b "$branch" https://git.savannah.gnu.org/git/"$package".git
 git clone --depth 1 https://git.savannah.gnu.org/git/gnulib.git
 
+(cd gnulib && git checkout 035e164bb016481ffe5e9f6b8be189bcdc41b0dd)
+
 # Apply patches.
 # (cd "$package" && patch -p1 < ../patches/)
 
