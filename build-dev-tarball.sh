6164a3decafd3715bfd8e2982e2d33920da87d38
Ileana Dumitrescu <ileanadumitrescu95@gmail.com>
2024-11-06 21:39:17 +0200

    [bsd] Test 0001-libtool.m4-Remove-OpenBSD-finish_cmds.patch

--- build-dev-tarball.sh
+++ build-dev-tarball.sh
@@ -39,7 +39,7 @@ git clone --depth 2 -b "$branch" https://git.savannah.gnu.org/git/"$package".git
 git clone --depth 1 https://git.savannah.gnu.org/git/gnulib.git
 
 # Apply patches.
-(cd "$package" && patch -p1 < ../patches/0001-libtool.m4-Update-hardcode_shlibpath_var-for-OpenBSD.patch)
+(cd "$package" && patch -p1 < ../patches/0001-libtool.m4-Remove-OpenBSD-finish_cmds.patch)
 
 export GNULIB_SRCDIR=`pwd`/gnulib
 cd "$package"
