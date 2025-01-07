e94965b9018289af0bee8189e3ec1887632b9df1
Ileana Dumitrescu <ileanadumitrescu95@gmail.com>
2025-01-07 21:18:02 +0200

    Remove applied patches

--- build-tarball.sh
+++ build-tarball.sh
@@ -39,8 +39,7 @@ git clone --depth 2 -b "$branch" https://git.savannah.gnu.org/git/"$package".git
 git clone --depth 1 https://git.savannah.gnu.org/git/gnulib.git
 
 # Apply patches.
-(cd "$package" && patch -p1 < ../patches/0001-ltmain.in-Update-information-supplied-to-user-for-ta.patch \
-               && patch -p1 < ../patches/0001-demo.at-Skip-test-if-deplibs_check_method-is-not-pas.patch)
+# (cd "$package" && patch -p1 < ../patches/<patch>)
 
 export GNULIB_SRCDIR=`pwd`/gnulib
 cd "$package"
