0e4b5fe847b51e47bd598eb2ae6046005279e57b
Ileana Dumitrescu <ileanadumitrescu95@gmail.com>
2025-08-22 16:48:15 +0300

    [all] Test 0001-Add-support-for-ML64.patch

--- build-dev-tarball.sh
+++ build-dev-tarball.sh
@@ -39,7 +39,8 @@ git clone --depth 2 -b "$branch" https://git.savannah.gnu.org/git/"$package".git
 git clone --depth 1 "${gnulib_url}"
 
 # Apply patches.
-(cd "$package" && patch -p1 < ../patches/Skip-test-option-parser.sh-for-ksh-shell-on-NetBSD.patch )
+(cd "$package" && patch -p1 < ../patches/Skip-test-option-parser.sh-for-ksh-shell-on-NetBSD.patch \
+               && patch -p1 < ../patches/0001-Add-support-for-ML64.patch)
 
 export GNULIB_SRCDIR=`pwd`/gnulib
 cd "$package"
