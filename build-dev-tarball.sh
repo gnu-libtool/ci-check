e449d2ecf698646e6966df367ce3d0513cd7e5da
Ileana Dumitrescu <ileanadumitrescu95@gmail.com>
2026-05-15 21:48:37 +0300

    Remove patches

--- build-dev-tarball.sh
+++ build-dev-tarball.sh
@@ -41,9 +41,7 @@ git clone --depth 2 -b "$branch" https://git.savannah.gnu.org/git/"$package".git
 git clone --depth 1 https://git.savannah.gnu.org/git/gnulib.git
 
 # Apply patches.
-(cd "$package" && patch -p1 < ../patches/Skip-test-option-parser.sh-for-ksh-shell-on-NetBSD.patch \
-               && patch -p1 < ../patches/0001-ltmain.in-Avoid-using-deprecated-MSVC-o-option.patch \
-               && patch -p1 < ../patches/0005-ltmain.in-overlinking-dependency_libs-when-linking-a.patch )
+(cd "$package" && patch -p1 < ../patches/Skip-test-option-parser.sh-for-ksh-shell-on-NetBSD.patch )
 
 export GNULIB_SRCDIR=`pwd`/gnulib
 cd "$package"
