2e124c0ceed8079b9f8053ca6c3e6c511796250d
Ileana Dumitrescu <ileanadumitrescu95@gmail.com>
2026-05-20 20:10:49 +0300

    [all] Test patches

--- build-dev-tarball.sh
+++ build-dev-tarball.sh
@@ -41,7 +41,10 @@ git clone --depth 2 -b "$branch" https://git.savannah.gnu.org/git/"$package".git
 git clone --depth 1 https://git.savannah.gnu.org/git/gnulib.git
 
 # Apply patches.
-(cd "$package" && patch -p1 < ../patches/Skip-test-option-parser.sh-for-ksh-shell-on-NetBSD.patch )
+(cd "$package" && patch -p1 < ../patches/Skip-test-option-parser.sh-for-ksh-shell-on-NetBSD.patch \
+               && patch -p1 < ../patches/0001-ltmain.in-ensure-old-libraries-are-not-archived.patch \
+               && patch -p1 < ../patches/0002-tests-extend-archive-in-archive-with-dependency-forw.patch \
+               && patch -p1 < ../patches/0003-ltmain.in-defer-warn-dependency-to-static-library.patch )
 
 export GNULIB_SRCDIR=`pwd`/gnulib
 cd "$package"
